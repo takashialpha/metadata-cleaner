f9436a83e5738ce2c12ec115d01f436e051a4150
takashialpha <takashialpha@protonmail.com>
2025-09-13 16:04:39 -0300

    merged gui and main. added file_chooser module.

---
+++ src/file_chooser.rs
@@ -0,0 +1,26 @@
+use gtk4::prelude::*;
+use gtk4::{FileDialog, Window};
+use gtk4::gio::File;
+use gtk4::glib::Error;
+
+pub struct FileDialogManager;
+
+impl FileDialogManager {
+    pub fn open_file<F: 'static + Fn(Option<String>)>(callback: F) {
+        let dialog = FileDialog::new();
+        dialog.set_title("Select a File");
+        dialog.set_modal(true);
+
+        dialog.open(
+            None::<&Window>,      // parent window
+            None::<&gtk4::gio::Cancellable>, // cancellable
+            move |result: Result<File, Error>| {
+                match result {
+                    Ok(file) => callback(Some(file.uri().to_string())),
+                    Err(_) => callback(None),
+                }
+            },
+        );
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,66 @@
 mod error;
-mod gui;
 mod metadata_info;
 mod metadata_reader;
 mod metadata_writer;
+mod file_chooser;
 
+use gtk4 as gtk;
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow, Button};
+use crate::file_chooser::FileDialogManager;
 use error::AppError;
-use gui::GuiApp;
 use metadata_info::MetadataInfo;
 use metadata_reader::MetadataReader;
-use metadata_writer::MetadataWriter;
+
+pub struct GuiApp {
+    app: gtk::Application,
+}
+
+impl GuiApp {
+    pub fn new(application_id: &str) -> Self {
+        let app = gtk::Application::builder()
+            .application_id(application_id)
+            .build();
+
+        Self { app }
+    }
+
+    fn setup(&self) {
+        self.app.connect_activate(|app| {
+            let window = ApplicationWindow::builder()
+                .application(app)
+                .title("Metadata Cleaner")
+                .build();
+
+            let button = Button::with_label("Open File");
+            button.connect_clicked(move |_| {
+                FileDialogManager::open_file(|file_option| {
+                    if let Some(file) = file_option {
+                        let raw_meta = MetadataReader::from_path(&file).unwrap_or_else(|e| {
+                            AppError::error_exit(e);
+                        });
+
+                        let fancy_meta = MetadataInfo::from(&raw_meta.raw_metadata);
+                        println!("{}", fancy_meta);
+                    } else {
+                        println!("No file selected");
+                    }
+                });
+            });
+
+            window.set_child(Some(&button));
+            window.present();
+        });
+    }
+
+    pub fn run(&self) -> glib::ExitCode {
+        self.setup();
+        self.app.run()
+    }
+}
 
 fn main() {
     let gui = GuiApp::new("org.takashialpha.metadatacleaner");
-    let exit_code = gui.run();
-    // std::process::exit(exit_code.into());
-
-    let file_path = "/home/takashi/Imagens/a3.jpg";
-
-    let raw_meta = MetadataReader::from_path(&file_path).unwrap_or_else(|e| {
-        AppError::error_exit(e);
-    });
-
-    let fancy_meta = MetadataInfo::from(&raw_meta.raw_metadata);
-    println!("{}", fancy_meta);
+    gui.run();
 }
+
